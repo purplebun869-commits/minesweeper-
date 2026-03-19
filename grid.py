fbb5539e51911cf5fae8ced86f128a6ed8abef06
apradhan7309 <purplebun869@gmail.com>
2026-03-18 22:47:42 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,7 +1,32 @@
+import pygame
 import random
 
 ROWS = 100
 COLS = 100
-MINES = 50
+MINES = 300
+CELL_SIZE = 8
+WIDTH = COLS * CELL_SIZE
+HEIGHT = ROWS * CELL_SIZE
 
-board = [[0 for __ in range(COLS)] for __ in range(ROWS)]
+#purple yayyyyy
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+board = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+visible = [[False for _ in range(COLS)] for _ in range(ROWS)]
+flagged = [[False for _ in range(COLS)] for _ in range(ROWS)]
+
+def place_mine():
+    count = 0
+    while count < MINES:
+        r = random.randint(0, ROWS-1)
+        c = random.randint(0, COLS-1)
+        if board[r][c] != "M":
+            board[r][c] = "M"
+            count +=1
+
+def count_mines(r, c):
+    dirs = [(-1, -1), (-1,0), (-1,-1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+    ct = 0
+    
